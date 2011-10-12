ba9cf95882d474877bccf4e09bfaef94e16adc66
Keita Tsutsui <keitanxkeitan@gmail.com>
2011-10-12 17:40:18 +0900

    JDBCを使ってデータベースファイルを生成するようにした。

--- src/jp/ac/titech/keitanxkeitan/xmlconverter/Database.java
+++ src/jp/ac/titech/keitanxkeitan/xmlconverter/Database.java
@@ -1,5 +1,9 @@
 package jp.ac.titech.keitanxkeitan.xmlconverter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class Database implements Element {
@@ -16,8 +20,38 @@ public class Database implements Element {
     public String encode() throws Exception {
         String code = "";
         
-        for (Table table : mTables) {
-            table.encode();
+        // load the sqlite-JDBC driver using the current class loader
+        Class.forName("org.sqlite.JDBC");
+        
+        Connection connection = null;
+        
+        try {
+            // create a database connection
+            // データベースファイルを明記する場合
+            // （例）/home/leo/work/mydatabase.db
+            // Connection connection = DriverManager.getConnection("jdbc:sqlite:/home/leo/work/mydatabase.db");
+            // 詳細は http://www.xerial.org/trac/Xerial/wiki/SQLiteJDBC を参照
+            connection = DriverManager.getConnection("jdbc:sqlite:gen/" + mName + ".db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            
+            for (Table table : mTables) {
+                String sql = table.encode();
+                statement.executeUpdate(sql);    
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e);
+            }
         }
         
         return code;
